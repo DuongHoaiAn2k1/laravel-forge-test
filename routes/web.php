1be766b688567cafb537d92c96ae59ef2d6a2666
DuongHoaiAn2k1 <dhan29112001@gmail.com>
2024-06-09 17:05:20 +0700

    Create forge route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/forge', function () {
+    return "My name is An. I am learning Laravel Forge.";
+});
