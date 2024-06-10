6735fa86b6566d37623b9547c3917433f2eea909
DuongHoaiAn2k1 <dhan29112001@gmail.com>
2024-06-10 21:41:00 +0700

    Update handle SendWelcomeEmail

--- app/Jobs/SendWelcomeEmail.php
+++ app/Jobs/SendWelcomeEmail.php
@@ -2,37 +2,32 @@
 
 namespace App\Jobs;
 
-use App\Mail\WelcomeEmail;
 use Illuminate\Bus\Queueable;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\WelcomeEmail;
 
 class SendWelcomeEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $user;
+    protected $user;
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
     public function __construct($user)
     {
         $this->user = $user;
     }
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
     public function handle()
     {
-        Mail::to($this->user->email)->send(new WelcomeEmail($this->user));
+        try {
+            Mail::to($this->user->email)->send(new WelcomeEmail($this->user));
+        } catch (\Exception $e) {
+            \Log::error("Failed to send welcome email: " . $e->getMessage());
+            throw $e; // Rethrow the exception to trigger retry logic
+        }
     }
 }
